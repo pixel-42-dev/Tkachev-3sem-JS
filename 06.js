fc5d9966321f3b4d358f4cd220747aeaca47df9d
Vadim Tkachev <vadim.tkachev.42@gmail.com>
2022-10-19 16:35:20 +0300

    1-10

---
+++ 06.js
@@ -0,0 +1,30 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+let a = "string";
+let b = 21;
+let c = true;
+let d = null;
+let e;
+let f = {};
+let g = new Array();
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
---
+++ 10.js
@@ -0,0 +1,14 @@
+/** ЗАДАЧА 10 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+
+let myFavoriteAnimal = 'Monkey'     // const заменен на let
+
+console.log(myFavoriteAnimal)
+    // 'Monkey'
+
+myFavoriteAnimal = 'Cat'            // const - убран
+
+console.log(myFavoriteAnimal)
+    // 'Cat'

2aca7918e8970663a050d5adc38d839a6e85974f
Vadim Tkachev <vadim.tkachev.42@gmail.com>
2022-10-20 11:10:53 +0300

    11-20

---
+++ 13.js
@@ -0,0 +1,17 @@
+/** ЗАДАЧА 13 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+let user = {
+    name: "Vadim",
+    surname: "Tkachev",
+    favoriteNumber: 21
+}
+console.log("My name is " + user.name + " " + user.surname + " and my favorite number is " + user.favoriteNumber);
---
+++ 17.js
@@ -0,0 +1,17 @@
+/** ЗАДАЧА 17 - Сравнение переменных
+ *
+ * 1. Сравните 2 переменные "myVariable1" и "myVariable2".
+ * Выведите в консоль "true", если значение "myVariable1" меньше или равно "myVariable2".
+ *
+ * 2. Преобразуйте обе переменные в числа перед сравнением.
+ */
+
+let myVariable1 = 10
+let myVariable2 = '5'
+
+console.log(myVariable1 == myVariable2);
+
+myVariable1 = '20'
+myVariable2 = 100
+
+console.log(Number(myVariable1) == Number(myVariable2));    
